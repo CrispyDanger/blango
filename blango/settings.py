5c87952ddbceaba396ccea5d22d3a5e835722074
Artem Yakovenko <a.yakovenko.ia01@kpi.ua>
2022-11-13 20:44:12 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -45,9 +45,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #global
+    "crispy_forms",
+    "crispy_bootstrap5",
     #local
     "blog",
+
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
