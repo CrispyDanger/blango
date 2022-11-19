e24e49c1d5a13f162dfb984d80ee2226f3792adb
Artem Yakovenko <a.yakovenko.ia01@kpi.ua>
2022-11-19 18:59:25 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,9 @@ logger = logging.getLogger(__name__)
 
 
 def index(request):
-  posts = Post.objects.filter(published_at__lte=timezone.now())
-  logger.debug("Got %d posts", len(posts))
-  return render(request, "blog/index.html", {"posts": posts})
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
+    return render(request, "blog/index.html", {"posts": posts})
 
 
 
